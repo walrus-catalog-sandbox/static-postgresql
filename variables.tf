64fa1ca50a70e709387ad504a485e14317f061d8
shanewxy <592491808@qq.com>
2023-11-15 12:21:58 +0800

    feat: postgresql static template

--- variables.tf
+++ variables.tf
@@ -23,3 +23,37 @@ EOF
   type        = map(any)
   default     = {}
 }
+
+variable "selector" {
+  description = "The selector, a map, which is used for dependencies or collaborations."
+  type        = map(any)
+  default     = {}
+}
+
+variable "endpoint_internal" {
+  description = "The internal endpoints of the resource."
+  type        = list(string)
+  default     = []
+}
+
+variable "endpoint_internal_readonly" {
+  description = "The internal readonly endpoints of the resource."
+  type        = list(string)
+  default     = []
+}
+
+variable "database" {
+  description = "The name of the PostgreSQL database to access."
+  type        = string
+}
+
+variable "username" {
+  description = "The username of the account to access the database."
+  type        = string
+}
+
+variable "password" {
+  description = "The password of the account to access the database."
+  type        = string
+  sensitive   = true
+}
